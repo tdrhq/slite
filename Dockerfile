24a2f150a194fea420180d4bb519917f100357db
Arnold Noronha <arnold@tdrhq.com>
2021-07-27 22:33:46 -0700

    We don't really need deadbeef
    GitOrigin-RevId: aa414056797ab725782b328dd41b6e305b9b9d2e

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,5 @@
 FROM debian:stable
 
 RUN apt-get update && apt-get install -y sbcl git-core
+ADD https://beta.quicklisp.org/quicklisp.lisp /quicklisp.lisp
+RUN chmod a+x quicklisp.lisp
--- jenkins.lisp
+++ jenkins.lisp
@@ -7,38 +7,13 @@
                                        (,(namestring *cwd*)
                                          ,(format nil "~abuild/asdf-cache/" *cwd*))))
 
-(push #P"third-party/deadbeef/" asdf:*central-registry*)
+(load "/quicklisp.lisp")
 
-(asdf:load-system :deadbeef)
-
-(deadbeef:register-external "https://github.com/vindarel/cl-str.git"
-                            "master")
-(deadbeef:register-external "https://github.com/sionescu/fiveam.git"
-                            "master")
-(deadbeef:register-external "https://github.com/edicl/cl-ppcre"
-                            "master")
-(deadbeef:register-external "https://github.com/edicl/cl-unicode"
-                            "master")
-(deadbeef:register-external "https://github.com/edicl/flexi-streams"
-                            "master")
-(deadbeef:register-external "https://github.com/trivial-gray-streams/trivial-gray-streams"
-                            "master")
-
-(deadbeef:register-external "https://github.com/rudolfochrist/cl-change-case"
-                            "master")
-(deadbeef:register-external "https://github.com/didierverna/asdf-flv"
-                            "master")
-
-(deadbeef:register-external "https://gitlab.common-lisp.net/alexandria/alexandria.git"
-                            "master")
-(deadbeef:register-external "https://github.com/gwkkwg/trivial-backtrace"
-                            "master")
-
-(deadbeef:prepare-externals "build/deadbeef/")
+(quicklisp-quickstart:install)
 
 (push #P "./" asdf:*central-registry*)
 
-(asdf:load-system :slite/tests)
+(ql:quickload :slite/tests)
 
 (unless (fiveam:run-all-tests)
   (uiop:quit 1))
