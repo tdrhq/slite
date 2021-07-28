7869fec885d62c01857a28071963fe69e0f8f12d
Arnold Noronha <arnold@tdrhq.com>
2021-07-28 11:02:19 -0700

    ..
    GitOrigin-RevId: 1a2d560950ca75e762cdc535d2b144d1dc0f7029

--- jenkins.lisp
+++ jenkins.lisp
@@ -1,16 +1,4 @@
-(require "asdf")
-
-(setf (uiop:getenv "HOME") "/cache-dir/")
-
-(defvar *cwd* "/cache-dir/")
-
-(load "/cache-dir/quicklisp/setup.lisp")
-
-
-(asdf:initialize-output-translations `(:output-translations
-                                       :inherit-configuration
-                                       (,(namestring *cwd*)
-                                         ,(format nil "~abuild/asdf-cache/" *cwd*))))
+(load "~/quicklisp/setup.lisp")
 
 (push #P "./" asdf:*central-registry*)
 
