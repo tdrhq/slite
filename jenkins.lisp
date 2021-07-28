946f7d5624d38857da3c0d7e73305a19331f3216
Arnold Noronha <arnold@tdrhq.com>
2021-07-27 18:00:02 -0700

    We're going to figure out how to make this scale
    GitOrigin-RevId: 73dbe2380da413fea96e6a1c6775f986be6ca780

--- jenkins.lisp
+++ jenkins.lisp
@@ -23,7 +23,10 @@
                             "master")
 (deadbeef:register-external "https://github.com/trivial-gray-streams/trivial-gray-streams"
                             "master")
+
 (deadbeef:register-external "https://github.com/rudolfochrist/cl-change-case"
+                            "master")
+(deadbeef:register-external "https://github.com/didierverna/asdf-flv"
                             "master")
 
 (deadbeef:register-external "https://gitlab.common-lisp.net/alexandria/alexandria.git"
